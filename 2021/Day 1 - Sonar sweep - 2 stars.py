8c5be0143316c83f79112a429277b873a2a7e1bd
Sebastian Golec <golec.sebastian@gmail.com>
2021-12-08 21:36:48 +0100

    Solve day 1 puzzle for 2 stars

---
+++ 2021/Day 1 - Sonar sweep - 2 stars.py
@@ -0,0 +1,17 @@
+path = '/Users/sebastiangolec/AdventOfCode/2021/input'
+
+input_file = open(path, 'r')
+counter = 0
+
+input = input_file.readlines()
+baseSum = int(input[0]) + int(input[1]) + int(input[2])
+i = 1
+
+while i < len(input) - 2:
+    scanSum = int(input[i]) + int(input[i+1]) + int(input[i+2])
+    if baseSum < scanSum:
+        counter = counter + 1
+    baseSum = scanSum
+    i = i+1
+
+print(counter)
