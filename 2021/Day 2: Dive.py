1068ca581c5630f3d86fe4a2cd9883032bf04bc2
Sebastian Golec <golec.sebastian@gmail.com>
2021-12-09 19:58:18 +0100

    Solve day 2 puzzle for 2 start

--- 2021/Day 2: Dive.py
+++ 2021/Day 2: Dive.py
@@ -1,11 +1,11 @@
-path = '/Users/sebastiangolec/AdventOfCode/2021/Day 2 input'
+path = "/Users/sebastiangolec/AdventOfCode/2021/Day 2 input"
 
 def calculatePosition(input):
     distance = 0
     depth = 0
 
     while len(input) > 0:
-        command = input.pop().split(" ")
+        command = input.pop(0).split(" ")
         match command[0]:
             case 'forward':
                 distance += int(command[1])
@@ -16,10 +16,27 @@ def calculatePosition(input):
 
     return distance, depth
 
+def calculatePositionWithAim(input):
+    distance = 0;
+    depth = 0;
+    aim = 0;
 
-input_file = open(path, 'r')
-input = input_file.readlines()
-position = calculatePosition(input)
+    while len(input) > 0:
+        command = input.pop(0).split(" ")
+        match command[0]:
+            case 'forward':
+                distance += int(command[1])
+                depth += aim * int(command[1])
+            case 'up':
+                aim -= int(command[1])
+            case 'down':
+                aim += int(command[1])
 
-print(position[0], position[1])
-print(int(position[0]) * int (position[1]))
+    return distance, depth
+
+input_file = open(path, 'r').readlines()
+position = calculatePosition(open(path, 'r').readlines())
+positionWithAim = calculatePositionWithAim(open(path, 'r').readlines())
+
+print(position[0], position[1], int(position[0]) * int (position[1]))
+print(positionWithAim[0], positionWithAim[1], int(positionWithAim[0]) * int (positionWithAim[1]))
