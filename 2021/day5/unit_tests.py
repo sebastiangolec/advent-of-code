9455e1ec5fe6b6a129666e7fecdfdb4854bcc7bb
Sebastian Golec <golec.sebastian@gmail.com>
2021-12-20 21:01:25 +0100

    Refactoring

--- 2021/day5/unit_tests.py
+++ 2021/day5/unit_tests.py
@@ -2,22 +2,28 @@ import unittest
 import vents
 
 
+def create_vent_line(x1: int, y1: int, x2: int, y2: int) -> vents.VentLine:
+    point1 = vents.Point(x1, y1)
+    point2 = vents.Point(x2, y2)
+
+    return vents.VentLine(point1, point2)
+
 class Test(unittest.TestCase):
     def test_parseInput(self):
         with open('test_input', 'r') as file:
             input_lines = file.readlines()
             result = vents.parse(input_lines)
             expected_result = [
-                vents.VentLine((0, 9), (5, 9)),
-                vents.VentLine((8, 0), (0, 8)),
-                vents.VentLine((9, 4), (3, 4)),
-                vents.VentLine((2, 2), (2, 1)),
-                vents.VentLine((7, 0), (7, 4)),
-                vents.VentLine((6, 4), (2, 0)),
-                vents.VentLine((0, 9), (2, 9)),
-                vents.VentLine((3, 4), (1, 4)),
-                vents.VentLine((0, 0), (8, 8)),
-                vents.VentLine((5, 5), (8, 2))
+                create_vent_line(0, 9, 5, 9),
+                create_vent_line(8, 0, 0, 8),
+                create_vent_line(9, 4, 3, 4),
+                create_vent_line(2, 2, 2, 1),
+                create_vent_line(7, 0, 7, 4),
+                create_vent_line(6, 4, 2, 0),
+                create_vent_line(0, 9, 2, 9),
+                create_vent_line(3, 4, 1, 4),
+                create_vent_line(0, 0, 8, 8),
+                create_vent_line(5, 5, 8, 2)
             ]
             self.assertEqual(result, expected_result)
             file.close()
@@ -25,42 +31,43 @@ class Test(unittest.TestCase):
 
     def test_filterOutDiagonalLines(self):
         lines = [
-            vents.VentLine((0, 9), (5, 9)),
-            vents.VentLine((8, 0), (0, 8)),
-            vents.VentLine((9, 4), (3, 4)),
-            vents.VentLine((2, 2), (2, 1)),
-            vents.VentLine((7, 0), (7, 4)),
-            vents.VentLine((6, 4), (2, 0)),
-            vents.VentLine((0, 9), (2, 9)),
-            vents.VentLine((3, 4), (1, 4)),
-            vents.VentLine((0, 0), (8, 8)),
-            vents.VentLine((5, 5), (8, 2))
+            create_vent_line(0, 9, 5, 9),
+            create_vent_line(8, 0, 0, 8),
+            create_vent_line(9, 4, 3, 4),
+            create_vent_line(2, 2, 2, 1),
+            create_vent_line(7, 0, 7, 4),
+            create_vent_line(6, 4, 2, 0),
+            create_vent_line(0, 9, 2, 9),
+            create_vent_line(3, 4, 1, 4),
+            create_vent_line(0, 0, 8, 8),
+            create_vent_line(5, 5, 8, 2)
         ]
 
         result = vents.filter_out_diagonal_lines(lines)
 
         expected_result = [
-            vents.VentLine((0, 9), (5, 9)),
-            vents.VentLine((9, 4), (3, 4)),
-            vents.VentLine((2, 2), (2, 1)),
-            vents.VentLine((7, 0), (7, 4)),
-            vents.VentLine((0, 9), (2, 9)),
-            vents.VentLine((3, 4), (1, 4))
+            create_vent_line(0, 9, 5, 9),
+            create_vent_line(9, 4, 3, 4),
+            create_vent_line(2, 2, 2, 1),
+            create_vent_line(7, 0, 7, 4),
+            create_vent_line(0, 9, 2, 9),
+            create_vent_line(3, 4, 1, 4)
         ]
         self.assertEqual(result, expected_result)
 
 
     def test_countOverlaps(self):
         input_lines = [
-            vents.VentLine((0, 9), (5, 9)),
-            vents.VentLine((9, 4), (3, 4)),
-            vents.VentLine((2, 2), (2, 1)),
-            vents.VentLine((7, 0), (7, 4)),
-            vents.VentLine((0, 9), (2, 9)),
-            vents.VentLine((3, 4), (1, 4))
+            create_vent_line(0, 9, 5, 9),
+            create_vent_line(9, 4, 3, 4),
+            create_vent_line(2, 2, 2, 1),
+            create_vent_line(7, 0, 7, 4),
+            create_vent_line(0, 9, 2, 9),
+            create_vent_line(3, 4, 1, 4)
         ]
 
-        result = vents.count_overlaps(input_lines)
+        vents_map = vents.map_vent_lines(input_lines)
+        result = vents.count_overlaps(vents_map)
         self.assertEqual(result, 5)
 
 
--- 2021/day5/vents.py
+++ 2021/day5/vents.py
@@ -1,16 +1,47 @@
-from collections import namedtuple
 import numpy
+from dataclasses import dataclass
 
-VentLine = namedtuple('VentLine', ['point1', 'point2'])
+
+@dataclass
+class Point:
+    x: int
+    y: int
+
+
+@dataclass
+class VentLine:
+    point1: Point
+    point2: Point
+
+
+    @classmethod
+    def fromLine(cls, line: str):
+        coords = line.split(" -> ")
+        coord1 = coords[0].split(',')
+        coord2 = coords[1].split(',')
+        point1 = Point(int(coord1[0]), int(coord1[1]))
+        point2 = Point(int(coord2[0]), int(coord2[1]))
+
+        return VentLine(point1, point2)
+
+
+    def is_horizontal(self) -> bool:
+        return self.point1.x != self.point2.x
+
+
+    def is_vertical(self) -> bool:
+        return self.point1.y != self.point2.y
+
+
+    def is_straight(self) -> bool:
+        return self.point1.x == self.point2.x or self.point1.y == self.point2.y
+
 
 def parse(input_lines: list[str]) -> list[VentLine]:
     vent_lines = []
 
     for line in input_lines:
-        points = line.split(" -> ")
-        point1 = points[0].split(',')
-        point2 = points[1].split(',')
-        vent_lines.append(VentLine((int(point1[0]), int(point1[1])), (int(point2[0]), int(point2[1]))))
+        vent_lines.append(VentLine.fromLine(line))
     
     return vent_lines
 
@@ -19,78 +50,51 @@ def filter_out_diagonal_lines(vent_lines: list[VentLine]) -> list[VentLine]:
     straight_lines = []
 
     for line in vent_lines:
-        if is_straight(line):
+        if line.is_straight():
             straight_lines.append(line)
     
     return straight_lines
 
 
-def is_straight(vent_line: VentLine) -> bool:
-    return vent_line.point1[0] == vent_line.point2[0] or vent_line.point1[1] == vent_line.point2[1]
+def map_vent_lines(vent_lines: list[VentLine]) -> numpy.ndarray:
+    vents_map = numpy.zeros(shape=(1000, 1000))
 
+    for vent_line in vent_lines:
+        point1 = vent_line.point1
+        point2 = vent_line.point2
 
-def count_overlaps(vent_lines: list[VentLine]) -> int:
-    max_x = find_max_x(vent_lines)
-    max_y = find_max_y(vent_lines)
-    vents_map = numpy.zeros(shape=(max_x+1, max_y+1))
-
-    for ventLine in vent_lines:
-        point1 = ventLine.point1
-        point2 = ventLine.point2
-
-        if point1[0] != point2[0]:
-            # horizontal
-            index = min(point1[0], point2[0])
-            while index <= max(point1[0], point2[0]):
-                vents_map[index, point1[1]] += 1
+        if vent_line.is_horizontal():
+            index = min(point1.x, point2.x)
+            while index <= max(point1.x, point2.x):
+                vents_map[index, point1.y] += 1
                 index += 1
-        elif point1[1] != point2[1]:
-            # vertical
-            index = min(point1[1], point2[1])
-            while index <= max(point1[1], point2[1]):
-                vents_map[point1[0], index] += 1
+        elif vent_line.is_vertical():
+            index = min(point1.y, point2.y)
+            while index <= max(point1.y, point2.y):
+                vents_map[point1.x, index] += 1
                 index += 1
+    
+    return vents_map
 
+
+def count_overlaps(vents_map: numpy.ndarray) -> int:
     overlaps = 0
-    for i in range(max_x+1):
-        for j in range(max_y+1):
-            map_xy = vents_map[i, j]
+
+    for i in range(1000):
+        for j in range(1000):
             if vents_map[i, j] > 1:
                 overlaps += 1
 
     return overlaps
 
 
-def find_max_x(vent_lines: list[VentLine]) -> int:
-    max_x = 0
-
-    for ventLine in vent_lines:
-        if ventLine.point1[0] > max_x:
-            max_x = ventLine.point1[0]
-        elif ventLine.point2[0] > max_x:
-            max_x = ventLine.point2[0]
-    
-    return max_x
-
-
-def find_max_y(vent_lines: list[VentLine]) -> int:
-    max_y = 0
-
-    for ventLine in vent_lines:
-        if ventLine.point1[1] > max_y:
-            max_y = ventLine.point1[1]
-        elif ventLine.point2[1] > max_y:
-            max_y = ventLine.point2[1]
-    
-    return max_y
-
-
 def count_overlaps_for_straight_vents() -> int:
     with open('input', 'r') as file:
-        input_lines = file.readlines()
-        vent_lines = parse(input_lines)
-        vent_lines = filter_out_diagonal_lines(vent_lines)
-        result = count_overlaps(vent_lines)
+        vent_lines = parse(file.readlines())
         file.close()
+
+        vent_lines = filter_out_diagonal_lines(vent_lines)
+        vents_map = map_vent_lines(vent_lines)
+        result = count_overlaps(vents_map)
 
         return result
