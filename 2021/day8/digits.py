3e94e791b7b505d3a0bdff876767ce88ecd452cf
Sebastian Golec <golec.sebastian@gmail.com>
2021-12-27 16:56:07 +0100

    Add docstrings, little code cleaning

--- 2021/day8/digits.py
+++ 2021/day8/digits.py
@@ -1,4 +1,8 @@
 class DigitsParser:
+    """
+    Class for parsing input for seven-segment display
+    """
+
     digits: list[tuple] = []
 
     def __init__(self, input_lines: list[str]):
@@ -8,6 +12,10 @@ class DigitsParser:
             self.digits.append((input_digits, output_digits))
 
     def count_unique_segment_digits(self) -> int:
+        """
+        Counts number of 1, 4, 7 and 8 digits in output part of given input
+        :return: Number of digits
+        """
         counter = 0
 
         for digit in self.digits:
@@ -18,6 +26,11 @@ class DigitsParser:
         return counter
 
     def has_unique_number_of_segments(self, digit: str):
+        """
+        1, 4, 7 and 8 are digits that have unique number of segments on when it's being displayed
+        :param digit: string input containing segments that are on
+        :return: information if digit is unique in matter of number of segments
+        """
         match (len(digit)):
             case 2 | 3 | 4 | 7:
                 return True
--- 2021/day8/unit_tests.py
+++ 2021/day8/unit_tests.py
@@ -6,7 +6,29 @@ class UnitTest(unittest.TestCase):
     def test_digits_with_unique_number_of_segments(self):
         with open('test_input', 'r') as file:
             digits_parser = DigitsParser(file.readlines())
+            file.close()
+
             self.assertEqual(26, digits_parser.count_unique_segment_digits())
+
+    # TODO reimplement according to changes in data structure of solution
+    # def test_decoding_small_example(self):
+    #     input_line = ['acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf']
+    #     expected_input_digits = [8, 5, 2, 3, 7, 9, 6, 4, 0, 1]
+    #     expected_output_digits = [5, 3, 5, 3]
+    #     digits_parser = DigitsParser(input_line)
+    #     result = digits_parser.decode()
+    #
+    #     self.assertEqual((expected_input_digits, expected_output_digits), result)
+    #
+    # def test_decoding_for_larger_sample(self):
+    #     with open('test_input', 'r') as file:
+    #         digits_parser = DigitsParser(file.readlines())
+    #         file.close()
+    #         expected_result = [8394, 9781, 1197, 9361, 4873, 8418, 4548, 1625, 8717, 4315]
+    #         result = digits_parser.decode()
+    #
+    #         for expected_number, result_number in zip(expected_result, result):
+    #             self.assertEqual(expected_number, result_number)
 
 
 if __name__ == '__main__':
